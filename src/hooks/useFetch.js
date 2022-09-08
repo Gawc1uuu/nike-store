59b905f0979c835d87a0e53ab3e4d68d3f9050fa
Jakub <gawlikjakub9@gmail.com>
2022-09-08 11:01:32 +0200

    dwadaw

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -14,9 +14,8 @@ export function useFetch(url) {
   useEffect(() => {
     const controller = new AbortController();
     setError(null);
-
+    setIsPending(true);
     const getShoes = () => {
-      setIsPending(true);
       try {
         fetch(url, { ...options, signal: controller.signal })
           .then((response) => {
@@ -35,8 +34,9 @@ export function useFetch(url) {
         }
         setError("Could not fetch the data");
       }
-      setIsPending(false);
     };
+
+    setIsPending(false);
 
     getShoes();
 
